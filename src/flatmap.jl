796cb885c40715a99d9a36d36badcc8e4622a3de
Alexander Plavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    simplify flatmap

--- src/flatmap.jl
+++ src/flatmap.jl
@@ -1,75 +1,28 @@
-function flatmap!(f::Function, out, A)
-    empty!(out)
-    for a in A
-        append!(out, f(a))
-    end
-    out
-end
+flatmap!(f::Function, out, A) = flatten!(out, mapview(f, A))
+flatmap!(f_out::Function, f_in::Function, out, A) = flatten!(out, mapview(a -> mapview(b -> f_in(a, b), f_out(a)), A))
 
-function flatmap!(f_out::Function, f_in::Function, out, A)
-    empty!(out)
-    for a in A
-        for b in f_out(a)
-            push!(out, f_in(a, b))
-        end
-    end
-    out
-end
+flatmap(f::Function, A) = flatten(mapview(f, A))
+flatmap(f_out::Function, f_in::Function, A) = flatten(mapview(a -> mapview(b -> f_in(a, b), f_out(a)), A))
+flatmap⁻(f_out, f_in::Function, A) = flatten(mapview(a -> mapview(b -> f_in(delete(a, f_out), b), f_out(a)), A))
 
-
-function flatmap(f::Function, A)
-    T = _eltype(Base.promote_op(f, _eltype(A)))
+function flatten(A)
+    T = _eltype(_eltype(A))
     it = iterate(A)
     if isnothing(it)
-        return _empty_from_type(Base.promote_op(f, _eltype(A)), T)
+        return _empty_from_type(_eltype(A), T)
     end
     afirst, state = it
     arest = Iterators.rest(A, state)
-    out = _similar_with_content(f(afirst), T)
-    for x in arest
-        _out = append!(out, f(x))
+    out = _similar_with_content(afirst, T)
+    flatten!(out, arest)
+end
+
+function flatten!(out, A) 
+    for a in A
+        _out = append!(out, a)
         @assert _out === out  # e.g. AxisKeys may return something else from append!
     end
-    return out
-end
-
-# rougly reduce(vcat, map(a -> map(b -> f_in(a, b), f_out(a)), A))
-function flatmap(f_out::Function, f_in::Function, A)
-    TO = _eltype(Base.promote_op(f_out, _eltype(A)))
-    T = Base.promote_op(f_in, _eltype(A), TO)
-    it = iterate(A)
-    if isnothing(it)
-        return _empty_from_type(Base.promote_op(f_out, _eltype(A)), T)
-    end
-    afirst, state = it
-    arest = Iterators.rest(A, state)
-    out = _similar_with_content(map(y -> f_in(afirst, y), f_out(afirst)), T)
-    for x in arest
-        for y in f_out(x)
-            push!(out, f_in(x, y))
-        end
-    end
-    return out
-end
-
-function flatmap⁻(f_out, f_in::Function, A)
-    TO = _eltype(Base.promote_op(f_out, _eltype(A)))
-    T = Base.promote_op(f_in, Base.promote_op(delete, _eltype(A), typeof(f_out)), TO)
-    it = iterate(A)
-    if isnothing(it)
-        return _empty_from_type(Base.promote_op(f_out, _eltype(A)), T)
-    end
-    afirst, state = it
-    afirst⁻ = delete(afirst, f_out)
-    arest = Iterators.rest(A, state)
-    out = _similar_with_content(map(y -> f_in(afirst⁻, y), f_out(afirst)), T)
-    for x in arest
-        x⁻ = delete(x, f_out)
-        for y in f_out(x)
-            push!(out, f_in(x⁻, y))
-        end
-    end
-    return out
+    out
 end
 
 
@@ -81,7 +34,3 @@ _empty_from_type(::Type, ::Type{T}) where {T} = T[]
 _empty_from_type(::Type{Union{}}, ::Type{T}) where {T} = T[]
 _empty_from_type(::Type{<:AbstractRange}, ::Type{T}) where {T} = T[]
 _empty_from_type(::Type{AT}, ::Type{T}) where {AT <: AbstractArray, T} = similar(similar(AT, 0), T)
-
-
-flatten(A) = flatmap(identity, A)
-flatten!(out, A) = flatmap!(identity, out, A)
